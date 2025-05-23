025bffd779bba0762c93a413fdefee51a4eeddf0
Akim Soulé <soule_akim@yahoo.fr>
2025-05-23 08:09:32 -0400

    dynamic

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import Link from "next/link";
 import { blogPosts, getPaginatedBlogPosts, getTotalBlogPages } from "@/lib/data";
 import BlogCard from "@/components/blog/BlogCard";
